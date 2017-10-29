1c89595ef6f90b3507a0185f838a6784a926305d
herquiloidehele <herquiloide@gmail.com>
2017-10-29 05:10:30 +0200

    adcionar rotas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,11 +4,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {TrabalhosComponent} from './trabalhos/trabalhos.component';
 import {NovoTrabalhoComponent} from './novo-trabalho/novo-trabalho.component';
 import {ModuleWithProviders} from '@angular/core';
+import {SignInComponent} from "./sign-in/sign-in.component";
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
 const APP_ROTAS: Routes = [
-  {path : '' , component: TrabalhosComponent},
-  {path: 'novo-trabalho', component: NovoTrabalhoComponent},
-  {path: 'trabalhos', component: TrabalhosComponent}
+    {path : '' , component: TrabalhosComponent},
+    {path: 'novo-trabalho', component: NovoTrabalhoComponent},
+    {path: 'trabalhos', component: TrabalhosComponent},
+    {path: 'signin', component: SignInComponent},
+    {path: 'signup', component: SignUpComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROTAS);
